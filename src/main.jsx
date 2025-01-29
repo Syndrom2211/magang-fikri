e4836c91eb1c3387ddfed0eb3a7327f97c04a078
fikrizaelan1 <fikrimuhamadzael@gmail.com>
2025-01-29 20:11:03 +0700

    add main

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./dist/css/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/main.css";
 
 import { BrowserRouter } from "react-router-dom";
 
